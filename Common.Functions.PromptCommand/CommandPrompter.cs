b5ad8e122277cc61bdfb87aa989b7cb831114468
Christopher Dorst <chris@dorst.email>
2017-12-27 14:40:41 -0600

    Adding CommandPrompter

---
+++ Common.Functions.PromptCommand/CommandPrompter.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace Common.Functions.PromptCommand
+{
+    public static class CommandPrompter
+    {
+        private const string CommandExe = "cmd.exe";
+
+        public static Process Prompt()
+            => Process.Start(new ProcessStartInfo
+            {
+                CreateNoWindow = true,
+                FileName = CommandExe,
+                RedirectStandardInput = true,
+                UseShellExecute = false
+            });
+    }
+}
